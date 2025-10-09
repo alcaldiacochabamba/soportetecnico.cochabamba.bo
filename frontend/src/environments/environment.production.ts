e68bb8cef4ac33f7db7d1e6fa2565840cd875ae4
yacel100 <barrientosnicolas67@gmail.com>
2025-10-09 12:01:16 -0400

    feat(producción): configuración Docker y entorno .env.prod con PostgreSQL

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+     baseUrl: "https://soportetecnicoapidev.cochabamba.bo/api/v1",
+     baseUrlAlt: "https://soportetecnicoapidev.cochabamba.bo", 
+     apiUrl: "https://soportetecnicoapidev.cochabamba.bo/api/v1",
+     production: true
+     
+};
